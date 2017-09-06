f75d4975424168abb9c88521057a3c87274cce39
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2017-09-07 00:06:11 +0300

    earth is added

---
+++ app/app.js
@@ -0,0 +1,49 @@
+var scene = (function() {
+    "use strict";
+    var scene = new THREE.Scene();
+    var renderer = window.WebGLRenderingContext ?
+        new THREE.WebGLRenderer({
+            alpha: true
+        }) : new THREE.CanvasRenderer();
+
+    var camera;
+    var earth;
+
+    function InitScene() {
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.getElementById('container').appendChild(renderer.domElement);
+
+        camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 1000);
+
+        camera.position.x = -80;
+        camera.position.z = 150;
+
+        scene.add(camera);
+
+
+        var texture = new THREE.TextureLoader().load("earth.jpg");
+
+        var material = new THREE.MeshBasicMaterial({
+            map: texture
+        });
+
+        var earth = new THREE.Mesh(new THREE.SphereGeometry(30, 32, 32), material);
+        earth.position.x = -90;
+        scene.add(earth);
+
+
+        render();
+    }
+
+    function render() {
+        earth.rotation.x += 0.0002;
+        earth.rotation.y += 0.02;
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+    }
+
+    return {
+        initScene: InitScene
+    }
+
+})();
